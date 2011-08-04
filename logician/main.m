70a2b4416daa3e7fa3ca353a07d373951fffb1bd
Hans Andersson <hans@Thor.local>
2011-08-04 06:12:39 -0400

    Initial Commit

---
+++ logician/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  logician
+//
+//  Created by Hans Andersson on 11/08/04.
+//  Copyright 2011 Ultramentem & Vigorware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
